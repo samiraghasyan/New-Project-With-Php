77c5c6f1c4681eaf4fee02863aa200f930ae777e
Reza Aghasian <samiraghasian76@gmail.com>
2025-09-29 22:57:18 +0330

    Add composer to project

--- config/app.php
+++ config/app.php
@@ -4,3 +4,8 @@
 const APP_NAME = 'MY_MVC';
 define('BASE_URL', 'https://' .$_SERVER['HTTP_HOST']);
 define('BASE_DIR',realpath(__DIR__.'/../'));
+
+$current_route = explode('?', $_SERVER['REQUEST_URI'])[0];
+$current_route = substr('/',1);
+
+define('CURRENT_ROUTE', $current_route);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f016b52c7c094b9e213c6ad35737728ef6d28dbe',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f016b52c7c094b9e213c6ad35737728ef6d28dbe',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
