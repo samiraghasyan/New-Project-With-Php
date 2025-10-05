6b6f3c348c25928730e47b94c2c6b79565945172
Reza Aghasian <samiraghasian76@gmail.com>
2025-10-05 23:54:14 +0330

    Orm model

---
+++ core/Database/ORM/Model.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Core\Database\ORM;
+
+use core\Database\Traits\HasQueryBuilder;
+
+abstract class Model
+{
+    use HasQueryBuilder;
+
+    protected $table;
+    protected $fillable = [];
+    protected $hidden = [];
+    protected $casts = [];
+    protected $primaryKey = 'id';
+    protected $createdAt = 'created_at';
+    protected $updatedAt = 'updated_at';
+    protected $deletedAt = null;
+    protected $collection = [];
+}
